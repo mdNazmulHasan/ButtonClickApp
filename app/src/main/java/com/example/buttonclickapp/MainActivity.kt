260c9da4bd9f56ff02a070fbef2312fc5c35231b
nhaucorp <nazmul@springrainit.com>
2019-03-07 16:46:35 +0600

    onclick event added

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.buttonclickapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private var userInput: EditText? = null
     private var button: Button? = null
     private var textView: TextView? = null
+    private var numTimesKilled = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +20,15 @@ class MainActivity : AppCompatActivity() {
         userInput = findViewById(R.id.editText)
         button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
+        textView?.text=""
+        button?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                numTimesKilled += 1
+                textView?.append("\n the button got clicked $numTimesKilled time")
+                if(numTimesKilled!=1){
+                    textView?.append("s")
+                }
+            }
+        })
     }
 }
