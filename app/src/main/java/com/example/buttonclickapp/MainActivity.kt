549decd867d7ae2b0dfe6189aa32b36b853eba41
nhaucorp <nazmul@springrainit.com>
2019-03-07 13:46:27 +0600

    findview added

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.buttonclickapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userInput = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
     }
 }
