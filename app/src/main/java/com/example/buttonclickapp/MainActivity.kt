cf40257240ae727e108507b3703fa56b6929cf82
mdnazmulhasan <najmul.hasan10@gmail.com>
2019-03-12 01:15:32 +0600

    cleared edit text in oncreate and button click

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -2,33 +2,28 @@ package com.example.buttonclickapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.text.method.ScrollingMovementMethod
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesKilled = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        userInput = findViewById(R.id.editText)
-        button = findViewById(R.id.button)
+        val userInput: EditText = findViewById(R.id.editText)
+        userInput.text.clear()
+        val button: Button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
-        textView?.text=""
-        button?.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                numTimesKilled += 1
-                textView?.append("\n the button got clicked $numTimesKilled time")
-                if(numTimesKilled!=1){
-                    textView?.append("s")
-                }
-            }
-        })
+        textView?.text = ""
+        textView?.movementMethod = ScrollingMovementMethod()
+        button.setOnClickListener {
+            textView?.append(userInput.text)
+            textView?.append("\n")
+            userInput.text.clear()
+        }
     }
 }
