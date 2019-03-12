f2b15864f5ce5bf21b97471277f6f6c5f520960d
nhaucorp <nazmul@springrainit.com>
2019-03-12 13:13:42 +0600

    data saved in bundle

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.buttonclickapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENT = "TextContent"
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
@@ -25,5 +28,18 @@ class MainActivity : AppCompatActivity() {
             textView?.append("\n")
             userInput.text.clear()
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        Log.e(TAG, "onsave instance state called")
+        super.onSaveInstanceState(outState)
+        outState?.putString(TEXT_CONTENT, textView?.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.e(TAG, savedInstanceState?.getString(TEXT_CONTENT, ""))
+        Log.e(TAG, "onrestore instance state called")
+        textView?.text = savedInstanceState?.getString(TEXT_CONTENT, "")
     }
 }
